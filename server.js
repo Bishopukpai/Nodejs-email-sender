43ed24f891b0a5e7c37d5125afd5afe0a84e3985
Bishop ukpai <bishopukpai0@gmail.com>
2023-12-22 11:47:38 +0100

    fifith commit

--- server.js
+++ server.js
@@ -14,8 +14,19 @@ app.use(bodyParser())
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "process.env.AUTH_EMAIL",
-        pass: "process.env.AUTH_PASS"
+        user: process.env.AUTH_EMAIL,
+        pass: process.env.AUTH_PASS
+    }
+})
+
+//test the nodemailer transporter 
+
+transporter.verify((error, success) =>{
+    if (error) {
+        console.log(error)
+    } else{
+        console.log("Ready for messages")
+        console.log(success)
     }
 })
 
