df13ff9143b5c91503c0bbd8bd11b60ffb7d1160
Bishop ukpai <bishopukpai0@gmail.com>
2023-12-22 10:30:36 +0100

    fourth commit

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('express').json
 require('dotenv').config()
-const port = 5000
+const port = 9000
 
 app.use(bodyParser())
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
