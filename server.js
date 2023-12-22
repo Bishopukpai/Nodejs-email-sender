65b96bf4547024572131691222ed68fe7eebbcd8
Bishop ukpai <bishopukpai0@gmail.com>
2023-12-22 10:58:22 +0100

    fourth commit

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('express').json
-require('dotenv').config()
+const nodemailer = require('nodemailer')
 const port = 9000
+require('dotenv').config()
 
 app.use(bodyParser())
 
+//config nodemailer
+//create a transporter with nodemailer. This transporter will carry
+//message from the port to destination
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "process.env.AUTH_EMAIL",
+        pass: "process.env.AUTH_PASS"
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
